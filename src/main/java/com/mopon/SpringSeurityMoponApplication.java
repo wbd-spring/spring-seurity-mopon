3d73010c65ac54a4155fa01590c3001de313c6ce
zgh <zgh@zgh-PC>
2020-02-07 14:26:56 +0800

    test

---
+++ src/main/java/com/mopon/SpringSeurityMoponApplication.java
@@ -0,0 +1,13 @@
+package com.mopon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeurityMoponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeurityMoponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mopon/SpringSeurityMoponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mopon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeurityMoponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
